89e49cafb6f662fe24f0318a6c1a482522845cb4
gouveiaarouemfe-tech <gouveiaarouemfe@gmail.com>
2025-09-22 13:26:43 -0300

    Update ArticleStructuredData.tsx

--- src/components/seo/ArticleStructuredData.tsx
+++ src/components/seo/ArticleStructuredData.tsx
@@ -1,4 +1,3 @@
-
 import Script from 'next/script';
 import { Post } from '@/lib/strapi';
 
@@ -8,7 +7,7 @@ interface ArticleStructuredDataProps {
 }
 
 export default function ArticleStructuredData({ post, imageUrl }: ArticleStructuredDataProps) {
-  const { title, content, publishedAt, updatedAt, seo_description, slug } = post;
+  const { title, content, publishedAt, updatedAt, seo_description, slug } = post.attributes;
 
   const articleSchema = {
     '@context': 'https://schema.org',
@@ -29,7 +28,7 @@ export default function ArticleStructuredData({ post, imageUrl }: ArticleStructu
         'url': 'https://climatbh-site-frontend.onrender.com/images/logo-climatbh.png',
       },
     },
-    'description': seo_description || content.substring(0, 160).replace(/[#*]/g, '') + '...',
+    'description': seo_description || content.substring(0, 160 ).replace(/[#*]/g, '') + '...',
     'mainEntityOfPage': {
       '@type': 'WebPage',
       '@id': `https://climatbh-site-frontend.onrender.com/blog/${slug}`,
@@ -40,8 +39,9 @@ export default function ArticleStructuredData({ post, imageUrl }: ArticleStructu
     <Script
       id="article-structured-data"
       type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(articleSchema) }}
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(articleSchema ) }}
     />
   );
 }
+
 
