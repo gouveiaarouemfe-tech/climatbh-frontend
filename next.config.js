e06015f65449aa84fb5a7de39d8800259966d880
gouveiaarouemfe-tech <gouveiaarouemfe@gmail.com>
2025-09-22 14:52:47 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,24 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   env: {
     NEXT_PUBLIC_STRAPI_API_URL: process.env.NEXT_PUBLIC_STRAPI_API_URL,
     NEXT_PUBLIC_API_TOKEN: process.env.NEXT_PUBLIC_API_TOKEN,
   },
+
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
       {
-        protocol: 'https',
-        hostname: 'climatbh-site-frontend.onrender.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "via.placeholder.com",
       },
       {
-        protocol: 'https',
-        hostname: 'my-blog-backend-9xz1.onrender.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "my-blog-backend-9xz1.onrender.com", // Adicionar o domínio do backend para imagens locais
       },
     ],
   },
