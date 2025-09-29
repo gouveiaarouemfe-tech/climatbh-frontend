3c1effc3177ce1a728639a7cc10ceefa71c956da
gouveiaarouemfe-tech <gouveiaarouemfe@gmail.com>
2025-09-28 21:49:01 -0300

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,7 +4,11 @@ import BlogClientPage from './BlogClientPage';
 export const dynamic = "force-dynamic";
 
 export default async function BlogPage() {
-  const posts = await getPosts();
+  console.log("BlogPage - Iniciando busca de posts...");
+  const posts = await getPosts(); // getPosts já foi corrigido para retornar res.data.dados
+  console.log("BlogPage - Posts recebidos:", posts);
+  console.log("BlogPage - Número de posts:", posts.length);
+  console.log("BlogPage - Primeiro post (se existir):", posts[0] ? JSON.stringify(posts[0], null, 2) : "Nenhum post encontrado");
 
   return (
     <BlogClientPage initialPosts={posts} />
